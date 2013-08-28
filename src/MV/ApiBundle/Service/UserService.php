e2a95969936cefd53de095ca5c9870ccba674365
Mitchel Verschoof <mitchel@future500.nl>
2013-08-28 11:20:22 +0200

    Fixed user

--- src/MV/ApiBundle/Service/UserService.php
+++ src/MV/ApiBundle/Service/UserService.php
@@ -4,19 +4,5 @@ namespace MV\ApiBundle\Service;
 
 class UserService extends EntityBaseService
 {
-    protected $serializer;
 
-    public function __construct($em, $serializer)
-    {
-        parent::__construct($em);
-
-        $this->serializer = $serializer;
-    }
-
-    public function findAll()
-    {
-        $usersArray = parent::findAll();
-
-        return $this->serializer->serialize($usersArray, 'json');
-    }
 }
